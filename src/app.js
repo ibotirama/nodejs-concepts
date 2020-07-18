49140cd5b3946f3ece636edec5f43c8241dd7fa6
Pedro Andrade <pedro.henrique1@gmail.com>
2020-07-18 21:52:46 +0300

    Implementation of repository endpoints

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
+const { json } = require("express");
 
 const app = express();
 
@@ -11,23 +12,49 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  let repository = {id:uuid(), title, url, techs, likes:0};
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex === -1){
+    response.statusMessage = 'Respository não encontrado.';
+    return response.status(400).end();
+  }
+  const {title, url, techs} = request.body;
+  repository = {id, title, url, techs, likes: repositories[repositoryIndex].likes};
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex === -1){
+    response.statusMessage = 'Respository não encontrado.';
+    return response.status(400).end();
+  }
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).end();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex === -1){
+    response.statusMessage = 'Respository não encontrado.';
+    return response.status(400).end();
+  }
+  const likes = repositories[repositoryIndex].likes++;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
